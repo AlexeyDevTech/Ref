345c65221148dac88efc649e274088cd946769b4
AlexeyDevTech <108923810+AlexeyDevTech@users.noreply.github.com>
2024-05-27 17:19:11 +0300

    init func part 1

--- RefTest.OSC/OSCImport.cs
+++ RefTest.OSC/OSCImport.cs
@@ -5,107 +5,135 @@ namespace RefTest.OSC
 
     public class OSCImport
     {
+        [DllImport("HTHardDll.dll", EntryPoint = "dsoSetUSBBus", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool dsoSetUSBBus(ushort DeviceIndex);
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSearchDevice", CharSet = CharSet.Auto ,CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSearchDevice(IntPtr pDevInfo);
+
+
+        [DllImport("HTHardDll.dll", EntryPoint = "dsoInitADCOnce", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoInitADCOnce(ushort DeviceIndex);
 
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTDeviceConnect", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTDeviceConnect(ushort nDeviceIndex);
 
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetCHPos", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSetCHPos(ushort nDeviceIndex, ushort nVoltDIV, ushort nPos, ushort nCH, ushort nCHMode);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetCHDirectLeverPos", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSetCHDirectLeverPos(ushort nDeviceIndex,ushort nPos,ushort nCH);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetVTriggerLevel", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSetVTriggerLevel(ushort nDeviceIndex, ushort nPos, ushort nSensitivity);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetHTriggerLength", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSetHTriggerLength(ushort nDeviceIndex, ref PCONTROLDATA pControl, ushort nCHMod);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetBufferSize", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSetBufferSize(ushort nDeviceIndex, ushort nBufferSize);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetCHAndTrigger", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSetCHAndTrigger(ushort nDeviceIndex, IntPtr pRelayControl, ushort nTimeDIV);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetCHAndTriggerDirect", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSetCHAndTriggerDirect(ushort nDeviceIndex, IntPtr pRelayControl, ushort uDirect, ushort nDriverCode);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetCHAndTriggerVB", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSetCHAndTriggerVB(ushort nDeviceIndex,ref ushort pCHEnable,ref ushort pCHVoltDIV,ref ushort pCHCoupling,ref ushort pCHBWLimit,ushort nTriggerSource,ushort nTriggerFilt,ushort nALT,ushort nTimeDIV );
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetTriggerAndSyncOutput", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSetTriggerAndSyncOutput(ushort nDeviceIndex, IntPtr pControl, ushort nTriggerMode, ushort nTriggerSlope, ushort nPWCondition, uint nPW, ushort nVideoStandard, ushort nVedioSyncSelect, ushort nVideoHsyncNumOption, ushort nSync);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetSampleRate", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTSetSampleRate(ushort nDeviceIndex, ushort nYTFormat, ref RELAYCONTROL pRelayControl, ref PCONTROLDATA pControl);
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTInitSDRam", CallingConvention = CallingConvention.Cdecl)]
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTStartCollectData", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTStartCollectData(ushort nDeviceIndex, ushort nStartControl);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTStartTrigger", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTStartTrigger(ushort nDeviceIndex);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTForceTrigger", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTForceTrigger(ushort nDeviceIndex);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetState", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTGetState(ushort nDeviceIndex);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetPackState", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetSDRamInit", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTGetPackState(ushort nDeviceIndex);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetData", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetScanData", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTGetData(ushort nDeviceIndex,ushort[] pCH1Data,ushort[] pCH2Data,ushort[] pCH3Data,ushort[] pCH4Data,ref PCONTROLDATA pControl);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTWriteCalibrationData", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoSDGetData", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoSDHTGetRollData", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoSDHTGetScanData", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTWriteCalibrationData(ushort nDeviceIndex, ushort[] pLevel, ushort nLen);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetRollData", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTOpenRollMode", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTCloseRollMode", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetPeakDetect", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTClosePeakDetect", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetHardFC", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetHardFC", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTResetCnter", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTStartRoll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTGetRollData(ushort nDeviceIndex, IntPtr pCH1Data, IntPtr pCH2Data, IntPtr pCH3Data, IntPtr pCH4Data, IntPtr pControl);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoGetHardVersion", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoUSBModeSetIPAddr", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoUSBModeGetIPAddr", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoOpenLan", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoOpenWIFIPower", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoResetWIFI", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoGetUSBModulVersion", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoSetUSBBus", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoSetSPIBus", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoSetHardInfo", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoGetHardInfo", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoWriteFlash", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoReadFlash", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoGetHardVersion(ushort DeviceIndex);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoGetDeviceName", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool dsoGetDeviceName(ushort DeviceIndex, [Out] byte[] pBuffer);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoGetDeviceSN", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool dsoGetDeviceSN(ushort DeviceIndex, [Out] byte[] pBuffer);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoGetPCBVersion", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool dsoGetPCBVersion(ushort DeviceIndex, [Out] byte[] pBuffer);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoGetDriverVersion", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoGetLANEnable", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoSetLANEnable", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoWriteIIC", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTWRAmpCali", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTRDAmpCali", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTWRADCCali", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTRDADCCali", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoGetDriverVersion(ushort DeviceIndex, [Out] byte[] pBuffer);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoInitHard", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool dsoInitHard(ushort DeviceIndex);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTADCCHModGain", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTADCCHModGain(ushort DeviceIndex, ushort nCHMod);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetAmpCalibrate", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetRamAndTrigerControl", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTSetAmpCalibrate(ushort nDeviceIndex,ushort nCHSet,ushort nTimeDIV,ref ushort nVoltDiv,ref ushort pCHPos);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetTrigerMode", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetVideoTriger", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetPulseTriger", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoGetInBufferWithoutOpen", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoGetInBuffer", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoSendOutBuffer", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoGetChmod", CallingConvention = CallingConvention.Cdecl)]
+        public static extern ushort dsoHTSetTrigerMode(ushort m_nDeviceIndex, ushort nTriggerMode, ushort nTriggerSlop, ushort nTriggerCouple);
+
+
         [DllImport("HTHardDll.dll", EntryPoint = "dsoGetSampleRate", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetIIC", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetSPI", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetUart", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetLinCan", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetSeriseData", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTSetSeriesTriggerCommon", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetCanDecode", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetLinDecode", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetUartDecode", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetSPIDecode", CallingConvention = CallingConvention.Cdecl)]
-        [DllImport("HTHardDll.dll", EntryPoint = "dsoHTGetIICDecode", CallingConvention = CallingConvention.Cdecl)]
-
-
+        public static extern float dsoGetSampleRate(ushort DeviceIndex);
 
 
         [DllImport("HTHardDll.dll", EntryPoint = "dsoGetFPGAVersion", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoGetFPGAVersion(ushort DeviceIndex);
+
 
         [DllImport("HTHardDll.dll", EntryPoint = "dsoHTReadCalibrationData", CallingConvention = CallingConvention.Cdecl)]
         public static extern ushort dsoHTReadCalibrationData(ushort nDeviceIndex, [In, Out] ushort[] pLevel, ushort nLen);
--- RefTest/Program.cs
+++ RefTest/Program.cs
@@ -20,7 +20,72 @@ namespace RefTest
                 {
                     ushort ver = GetVersion();
                     Console.WriteLine(ver);
-                    Console.ReadKey();
+                    // Console.ReadKey();
+
+                    var f = OSCImport.dsoSetUSBBus(0);
+                    if (OSCImport.dsoInitHard(0))
+                    {
+                        if (OSCImport.dsoHTADCCHModGain(0, 1) != 0)
+                        {
+                            /*
+                             * ZERO_FLAG				       0xFBCF
+                             * 
+                             * 
+                             * 
+                             * CAL_LEVEL_LEN           ZEROCALI_LEN +1                         = 577 
+                             * ZEROCALI_LEN            (ZEROCALI_PER_CH_LEN*MAX_CH_NUM)        = 576
+                             * ZEROCALI_PER_CH_LEN     (ZEROCALI_PER_VOLT_LEN*MAX_VOLTDIV_NUM) = 144
+                             * MAX_VOLTDIV_NUM         12
+                             * ZEROCALI_PER_VOLT_LEN   12
+                             * MAX_CH_NUM              4
+                             */
+                            ushort[] m_CalLevel = new ushort[577];
+                            OSCImport.dsoHTReadCalibrationData(0, m_CalLevel, 577);
+
+
+                            if (m_CalLevel[m_CalLevel.Length - 1] != 0xFBCF)
+                            {
+                                //re-calibrate
+                                for (int i = 0; i < 577; i++)
+                                {
+                                    int n_volt = (i % 144) / 12;
+                                    if (n_volt == 5 || n_volt == 8 || n_volt == 11)
+                                    {
+                                        switch ((i % 144) % 12)
+                                        {
+                                            case 0:
+                                                m_CalLevel[i] = 16602;
+                                                break;
+                                            case 1:
+                                                m_CalLevel[i] = 60111;
+                                                break;
+                                            case 2:
+                                                m_CalLevel[i] = 17528;
+                                                break;
+                                            case 3:
+                                                m_CalLevel[i] = 59201;
+                                                break;
+                                            case 4:
+                                                m_CalLevel[i] = 17710;
+                                                break;
+                                            case 5:
+                                                m_CalLevel[i] = 58900;
+                                                break;
+                                            default:
+                                                m_CalLevel[i] = 0;
+                                                break;
+
+
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+
+                    }
+
+
 
                     //ushort len = 128;
                     //var arr = new ushort[len];
