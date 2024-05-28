f85a68473ce3e8b8d30a8d6de731ed039732e5cc
alexey <strannik9717@hotmail.com>
2024-05-28 22:36:11 +0300

    clean

--- RefTest/Program.cs
+++ RefTest/Program.cs
@@ -35,38 +35,6 @@ namespace RefTest
                             inst.Start();
                         }
                     };
-
-
-
-
-                        //Stopwatch sw = new Stopwatch();
-                        //await Task.Factory.StartNew(() =>
-                        //{
-                        //    sw.Start();
-                        //    int conter = 100;
-                        //    while (conter > 0)
-                        //    {
-                        //        OSCImport.dsoHTStartCollectData(0, 5);
-
-                        //        while ((OSCImport.dsoHTGetState(0) & 0x02) == 0)
-                        //        {
-                        //            Task.Delay(10);
-                        //        }
-                        //        ushort[] ch1 = new ushort[65536];
-                        //        ushort[] ch2 = new ushort[65536];
-                        //        ushort[] ch3 = new ushort[65536];
-                        //        ushort[] ch4 = new ushort[65536];
-                        //        var res = OSCImport.dsoHTGetData(0, ch1, ch2, ch3, ch4, ref OSCControl.Instance.stControl);
-                        //        conter--;
-                        //        Console.WriteLine($"iterations lost: {conter}, elapsed: {sw.Elapsed}");
-                        //    }
-                        //    sw.Stop();
-                        //    Console.WriteLine(sw.Elapsed);
-                        //});
-                        //ushort len = 128;
-                        //var arr = new ushort[len];
-
-                        //var suc = OSCImport.dsoHTReadCalibrationData(0, arr, len);
                     }
             }
             catch (AccessViolationException)
